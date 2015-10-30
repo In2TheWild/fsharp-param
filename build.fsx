8a1a4ef40c794f3a8e7524934c9f9c6b14918fcd
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-10-30 21:15:11 +0700

    decompile using ilspy's decompiler

--- build.fsx
+++ build.fsx
@@ -1,14 +1,41 @@
 
 #I "packages/FAKE/tools/"
+#I "packages/ICSharpCode.Decompiler/lib/net40/"
+#I "packages/ICSharpCode.NRefactory/lib/net40/"
+#I "packages/Mono.Cecil/lib/net40"
+
+#r "ICSharpCode.Decompiler"
+#r "Mono.Cecil.dll"
 #r "packages/FAKE/tools/FakeLib.dll"
 
 open Fake.FscHelper
 open Fake
 open Fake.FileHelper
 open Fake.CscHelper
+open ICSharpCode.Decompiler.Ast
+open ICSharpCode.Decompiler
+open System
+open System.Text
+open System.IO
 
 CreateDir "release"
 "packages/FAKE/tools/FSharp.Core.dll" |> CopyFile "release/FSharp.Core.dll"
+
+Target "dc" (fun _ ->
+        try
+            let pathToAssembly = "release/lib.dll"
+            let def = Mono.Cecil.AssemblyDefinition.ReadAssembly(pathToAssembly)
+
+            let dc = AstBuilder(DecompilerContext(def.MainModule))
+            dc.AddAssembly(def);
+            //Helpers.RemoveCompilerAttribute().Run(decompiler.SyntaxTree);
+
+            let output = new StringWriter()
+            dc.GenerateCode(PlainTextOutput(output))
+            let str = output.ToString() + "Hello"
+            printfn "%s" str
+        with err -> printfn "%A" err
+)
 
 Target "cc" (fun _ ->
         ["src/cs/Program.cs"]
@@ -27,7 +54,8 @@ Target "cf" (fun _ ->
                 FscTarget = FscTarget.Library} )
     )
 
-RunTargetOrDefault fsi.CommandLineArgs.[1]
+//RunTargetOrDefault fsi.CommandLineArgs.[1]
+RunTargetOrDefault "dc"
 
 (*
 [<EntryPoint>]
